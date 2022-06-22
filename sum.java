c874e62c2a144a96ea9a7c3d2f57aa16853139ef
Harsh12chauhan <harshchauhan0225@gmail.com>
2022-06-22 19:01:56 +0530

    programs of printing hello and sum of 3 numbers

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+     public static void main(String[] args){
+    System.out.print("The sum of three numbers is : ");
+    int num1 = 2 ;
+    int num2 = 2 ;
+    int num3 =  2;
+    int sum = num1 + num2 + num3 ;
+    System.out.println(sum);
+    }
+}
